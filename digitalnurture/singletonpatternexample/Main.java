8bca3ae2de4e502d7c7b784218d03758026e8138
Tarun-2005 <taruntarunoff@gmail.com>
2025-06-20 00:33:19 +0530

    Update Main.java

--- digitalnurture/singletonpatternexample/Main.java
+++ digitalnurture/singletonpatternexample/Main.java
@@ -6,6 +6,12 @@ public class Main {
     public static void main(String[] args) {
         Logger logger = Logger.getinstance();
         logger.displaymsg();
+        Logger anotherLogger = Logger.getinstance();
+        if (logger == anotherLogger) {
+            System.out.println("Both logger instances are the same.");
+        } else {
+            System.out.println("Logger instances are different.");
+        }
     }
     
 }
